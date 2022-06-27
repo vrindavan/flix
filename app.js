e3919330183029076bd58dc860f8079a64618394
Amay <brajbliss@gmail.com>
2022-06-28 01:40:51 +0530

    responsive slider idk how

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ arrows.forEach((arrow, i) => {
 	let clickCounter = 0;
 
 	arrow.addEventListener('click', () => {
+		const ratio = Math.floor(window.innerWidth / 270);
 		clickCounter++;
-		if (itemNumber - (4 + clickCounter) > 0) {
+		if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
 			movieLists[i].style.transform = `translateX(${
 				movieLists[i].computedStyleMap().get('transform')[0].x.value -
 				300
@@ -19,6 +20,8 @@ arrows.forEach((arrow, i) => {
 		}
 	});
 });
+
+// Toggle
 
 const ball = document.querySelector('.toggle-ball');
 const items = document.querySelectorAll(
