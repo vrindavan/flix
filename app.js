6e18844090bb76120482a0ff069439bbd132b130
Amay <brajbliss@gmail.com>
2022-06-28 01:17:13 +0530

    dark mode wohooo

--- app.js
+++ app.js
@@ -19,3 +19,15 @@ arrows.forEach((arrow, i) => {
 		}
 	});
 });
+
+const ball = document.querySelector('.toggle-ball');
+const items = document.querySelectorAll(
+	'.container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle'
+);
+
+ball.addEventListener('click', () => {
+	items.forEach((item) => {
+		item.classList.toggle('active');
+	});
+	ball.classList.toggle('active');
+});
